d0f06dfed0b37f7746f291e4f119e4276bf76085
m1ckyb <m1ckyb@users.noreply.github.com>
2022-06-18 18:05:46 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,20 @@
 /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=uptime
 cd /root/uptime-kuma
+
+if [ -z "$hc" ]
+then
+      echo "\$hc is NULL"
+else
+      echo "\$hc is NOT NULL"
+
+    cat > /etc/periodic/15min/hc <<EOF
+    curl -m 10 --retry 5 $hc
+    EOF
+    chmod +x /etc/periodic/15min/hc
+fi
+
 npm run setup
 # Start Server
 node server/server.js
-#pm2 start server/server.js --name uptime-kuma
+
